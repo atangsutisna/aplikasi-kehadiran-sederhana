f9e96e9a9623c33938b078d00bd9d3ea8f68170d
Atang Sutisna <atang.sutisna.87@gmail.com>
2017-02-23 22:41:36 +0000

    styling new form user

--- application/views/user/form.php
+++ application/views/user/form.php
@@ -1,69 +1,77 @@
-<p>&nbsp;</p>
-<?php 
-  $formAttr = array("class" => "form-horizontal");
-  echo !isset($user) ? form_open("user/insert", $formAttr) : form_open("user/update", $formAttr);
-?>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <h4>FORM PENGGUNA</h4>
-      <?php echo validation_errors() ?>
-      <?php 
-        if ($this->session->flashdata('notif') != NULL) {
-            echo "<div class='alert alert-info'>";
-            echo $this->session->flashdata('notif');
-            echo "</div>";
-        }
-      ?>
-    </div>
-  </div>
-  <div class="form-group">
-    <label class="col-sm-2 control-label">Username</label>
-    <div class="col-sm-4">
-      <input type="text" class="form-control" placeholder="Username" name="username" 
-      value="<?php echo isset($user) ? $user->username : ""?>"
-      <?php echo isset($user) ? 'disabled' : '' ?>>
-    </div>
-  </div>
-  <div class="form-group">
-    <label class="col-sm-2 control-label">Nama Staff</label>
-    <div class="col-sm-5">
-      <?php
-        $staffOpt = array('0' => 'Pilih Staff');
-        foreach($staffs as $staff) {
-          $staffOpt[$staff->id] = $staff->nama;  
-        }
-        echo form_dropdown('id_pengguna', $staffOpt, isset($user) ? $user->id_pengguna : '0');
-      ?>              
-    </div>
-  </div>
-  <div class="form-group">
-    <label class="col-sm-2 control-label">Peran</label>
-    <div class="col-sm-4">
-      <?php
-        $peranOpt = array(
-          '0' => 'Pilih Peran',
-          'ADMINISTRATOR' => 'ADMINISTRATOR',
-          'STAFF' => 'STAFF'
-        );
-        echo form_dropdown('peran', $peranOpt, isset($user) ? $user->peran : '0');
-      ?>
-    </div>
-  </div>
-  <div class="form-group">
-    <label class="col-sm-2 control-label">Password</label>
-    <div class="col-sm-4">
-      <input type="text" class="form-control" placeholder="Username" name="password">     
-    </div>
-  </div>
-  <div class="form-group">
-    <label class="col-sm-2 control-label">Retype Password</label>
-    <div class="col-sm-4">
-      <input type="text" class="form-control" placeholder="Username" name="retype_password">     
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-primary">Simpan</button>
-    </div>
-  </div>
-<?php echo form_close() ?>
+<div class="col-lg-12">
+  <div class="box box-info">
+        <div class="box-header with-border">
+            FORM PENGGUNA
+            <div class="box-tools">
+                <?php echo anchor('user', '<< Kembali', array('class' => 'btn btn-primary')) ?>
+            </div>
+            <br/><br/>
+        </div>
+        <div class="box-body no-padding">
+            <?php 
+              $formAttr = array("class" => "form-horizontal");
+              echo !isset($user) ? form_open("user/insert", $formAttr) : form_open("user/update", $formAttr);
+                      
+              echo validation_errors();
+            
+              if ($this->session->flashdata('notif') != NULL) {
+                  echo "<div class='alert alert-info'>";
+                  echo $this->session->flashdata('notif');
+                  echo "</div>";
+              }
+            ?>          
+            <div class="form-group">
+              <label class="col-sm-2 control-label">Username</label>
+              <div class="col-sm-4">
+                <input type="text" class="form-control" placeholder="Username" name="username" 
+                value="<?php echo isset($user) ? $user->username : ""?>"
+                <?php echo isset($user) ? 'disabled' : '' ?>>
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-sm-2 control-label">Nama Staff</label>
+              <div class="col-sm-5">
+                <?php
+                  $staffOpt = array('0' => 'Pilih Staff');
+                  foreach($staffs as $staff) {
+                    $staffOpt[$staff->id] = $staff->nama;  
+                  }
+                  echo form_dropdown('id_pengguna', $staffOpt, isset($user) ? $user->id_pengguna : '0');
+                ?>              
+              </div>
+            </div>            
+            <div class="form-group">
+              <label class="col-sm-2 control-label">Peran</label>
+              <div class="col-sm-4">
+                <?php
+                  $peranOpt = array(
+                    '0' => 'Pilih Peran',
+                    'ADMINISTRATOR' => 'ADMINISTRATOR',
+                    'STAFF' => 'STAFF'
+                  );
+                  echo form_dropdown('peran', $peranOpt, isset($user) ? $user->peran : '0');
+                ?>
+              </div>
+            </div>  
+            <div class="form-group">
+              <label class="col-sm-2 control-label">Password</label>
+              <div class="col-sm-4">
+                <input type="text" class="form-control" placeholder="Username" name="password">     
+              </div>
+            </div>            
+            <div class="form-group">
+              <label class="col-sm-2 control-label">Retype Password</label>
+              <div class="col-sm-4">
+                <input type="text" class="form-control" placeholder="Username" name="retype_password">     
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-primary">Simpan</button>
+              </div>
+            </div>            
+            <?php echo form_close() ?>            
+        </div>
+        <!-- end of box body -->
+  </div>    
+</div>  
--- application/views/user/view.php
+++ application/views/user/view.php
@@ -1,33 +1,44 @@
-<h1>Daftar Pengguna</h1>
-<?php 
-if ($this->session->flashdata('notif') != NULL) {
-    echo "<div class='alert alert-info'>";
-    echo $this->session->flashdata('notif');
-    echo "</div>";
-}
-?>
-<?php echo anchor('user/new_form', 'Tambah Pengguna Baru') ?>
-<table class="table">
-    <thead>
-        <tr>
-            <th>username</th>
-            <th>Nama Lenkap</th>            
-            <th>Peran</th>
-            <th>Status</th>
-            <th>#</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach($users as $user) : ?>
-        <tr>
-            <td><?php echo $user->username ?></td>
-            <td><?php echo $user->nama ?></td>
-            <td><?php echo $user->peran ?></td>
-            <td><?php echo $user->status == true ? 'AKTIF' : 'NON AKTIF' ?></td>
-            <td>
-                <?php echo anchor('user/edit/'. $user->id_pengguna , 'Edit') ?>  
-            </td>
-        </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+<div class="col-lg-12">
+    <div class="box box-info">
+        <div class="box-header with-border">
+            DAFTAR PENGGUNA
+            <div class="box-tools">
+                <?php echo anchor('user/new_form', 'Tambah Pengguna Baru', array('class' => 'btn btn-primary')) ?>
+            </div>
+            <br/><br/>
+        </div>
+        <div class="box-body no-padding">
+            <?php 
+            if ($this->session->flashdata('notif') != NULL) {
+                echo "<div class='alert alert-info'>";
+                echo $this->session->flashdata('notif');
+                echo "</div>";
+            }
+            ?>      
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>username</th>
+                        <th>Nama Lenkap</th>            
+                        <th>Peran</th>
+                        <th>Status</th>
+                        <th>#</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($users as $user) : ?>
+                    <tr>
+                        <td><?php echo $user->username ?></td>
+                        <td><?php echo $user->nama ?></td>
+                        <td><?php echo $user->peran ?></td>
+                        <td><?php echo $user->status == true ? 'AKTIF' : 'NON AKTIF' ?></td>
+                        <td>
+                            <?php echo anchor('user/edit/'. $user->id_pengguna , 'Edit') ?>  
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>            
+        </div>
+    </div>
+</div>
