b9afbb5838a2194a773d0dc75066b0ce74aa8f80
Atang Sutisna <atang.sutisna.87@gmail.com>
2018-11-06 15:10:07 +0700

    new login form

--- application/views/logged_in.php
+++ application/views/logged_in.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title>Form Logged In</title>
+    <title>Presence App | Form Login</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <link rel="stylesheet" href="<?php echo base_url('css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?php echo base_url('css/font-awesome/css/font-awesome.min.css') ?>">
